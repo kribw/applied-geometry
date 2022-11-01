65ecdd34f367b80e6e4c886130a96f33fb377581
Kristoffer Berg Wilhelmsen <kwi059@uit.no>
2022-11-01 15:09:25 +0100

    Added BSpline to scenario

--- demo/scenario.cpp
+++ demo/scenario.cpp
@@ -13,23 +13,27 @@
 // qt
 #include <QQuickItem>
 
+// applied_geometry
+#include "applied_geometry/bspline.h"
+
 template <typename T>
-inline std::ostream &operator<<(std::ostream &out, const std::vector<T> &v) {
+inline std::ostream& operator<<(std::ostream& out, const std::vector<T>& v)
+{
   out << v.size() << std::endl;
-  for (uint i = 0; i < v.size(); i++)
-    out << " " << v[i];
+  for (uint i = 0; i < v.size(); i++) out << " " << v[i];
   out << std::endl;
   return out;
 }
 
-FEMObject *_femObject;
+// FEMObject *_femObject;
 
-void Scenario::initializeScenario() {
+void Scenario::initializeScenario()
+{
 
   // Insert a light
   GMlib::Point<GLfloat, 3> init_light_pos(2.0, 4.0, 10);
-  GMlib::PointLight *light =
-      new GMlib::PointLight(GMlib::GMcolor::white(), GMlib::GMcolor::white(),
+  GMlib::PointLight*       light
+    = new GMlib::PointLight(GMlib::GMcolor::white(), GMlib::GMcolor::white(),
                             GMlib::GMcolor::white(), init_light_pos);
   light->setAttenuation(0.8f, 0.002f, 0.0008f);
   this->scene()->insertLight(light, false);
@@ -38,8 +42,8 @@ void Scenario::initializeScenario() {
   this->scene()->insertSun();
 
   // Default camera parameters
-  int init_viewport_size = 600;
-  GMlib::Point<float, 3> init_cam_pos(0.0f, 0.0f, 0.0f);
+  int                     init_viewport_size = 600;
+  GMlib::Point<float, 3>  init_cam_pos(0.0f, 0.0f, 0.0f);
   GMlib::Vector<float, 3> init_cam_dir(0.0f, 1.0f, 0.0f);
   GMlib::Vector<float, 3> init_cam_up(1.0f, 0.0f, 0.0f);
 
@@ -50,10 +54,10 @@ void Scenario::initializeScenario() {
   proj_rcpair.camera->rotateGlobal(GMlib::Angle(-45),
                                    GMlib::Vector<float, 3>(1.0f, 0.0f, 0.0f));
   proj_rcpair.camera->translateGlobal(
-      GMlib::Vector<float, 3>(0.0f, -20.0f, 20.0f));
+    GMlib::Vector<float, 3>(0.0f, -20.0f, 20.0f));
   scene()->insertCamera(proj_rcpair.camera.get());
   proj_rcpair.renderer->reshape(
-      GMlib::Vector<int, 2>(init_viewport_size, init_viewport_size));
+    GMlib::Vector<int, 2>(init_viewport_size, init_viewport_size));
 
   /***************************************************************************
    *                                                                         *
@@ -65,22 +69,27 @@ void Scenario::initializeScenario() {
   mm.set(45.0);
 
   // Instantiate FEMObject and run demo
-  _femObject = new FEMObject();
-  _femObject->toggleDefaultVisualizer();
-  _femObject->demoRandom();
-  //  _femObject->demoRegular();
-  this->scene()->insert(_femObject);
+  //  _femObject = new FEMObject();
+  //  _femObject->toggleDefaultVisualizer();
+  //  _femObject->demoRandom();
+  //  this->scene()->insert(_femObject);
+  auto bspline = new kwi::BSpline<float>();
+  bspline->toggleDefaultVisualizer();
+  bspline->setMaterial(GMlib::GMmaterial::polishedGreen());
+  bspline->sample(50, 0);
+  this->scene()->insert(bspline);
 }
 
 void Scenario::cleanupScenario() {}
 
-void Scenario::callDefferedGL() {
+void Scenario::callDefferedGL()
+{
   // For some reason this does not replot objects properly
-  //    GMlib::Array<const GMlib::SceneObject *> e_obj;
-  //    this->scene()->getEditedObjects(e_obj);
-  //    for (int i = 0; i < e_obj.getSize(); i++)
-  //      //    if (e_obj(i)->isVisible())
-  //      e_obj[i]->replot();
+  GMlib::Array<const GMlib::SceneObject*> e_obj;
+  this->scene()->getEditedObjects(e_obj);
+  for (int i = 0; i < e_obj.getSize(); i++)
+    //    if (e_obj(i)->isVisible())
+    e_obj[i]->replot();
 
-  _femObject->replot();
+  //  _femObject->replot();
 }
